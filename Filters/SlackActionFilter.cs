5787daad6d5160b6075851d78f35adc39bca375b
Ryan Fahsel <rfahsel3@Katherines-MacBook-Pro.local>
2017-10-03 23:44:57 -0700

    Forgot to add actual filter

---
+++ Filters/SlackActionFilter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Ticketer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Ticketer.Filters {
+    public class SlackActionFilter : ActionFilterAttribute {
+        
+        private IConfiguration config;
+
+        public SlackActionFilter(IConfiguration config) {
+            this.config = config;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            bool success = false;
+            if (context.ActionArguments.Keys.Contains("slackRequest")) {
+                SlackRequest sr = context.ActionArguments["slackRequest"] as SlackRequest;
+                if (sr.token == config.GetValue<string>("SlackVerificationToken")) {
+                    success = true;
+                }
+            }
+
+            if (!success) {
+                context.HttpContext.Response.StatusCode = 403;
+                context.Result = new ContentResult() {Content = "Bad Slack verification token" };
+            }
+        }
+    }
+}
