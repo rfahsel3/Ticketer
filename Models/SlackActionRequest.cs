b3324aa02149d20dc2cd103a17e0612fa2138270
Ryan Fahsel <rfahsel3@Katherines-MacBook-Pro.local>
2017-10-04 22:20:13 -0700

    Fixed bug where action request was not being deserialized. Fixed by disabling some other fields that were not strings, but comlex objects. Since we don't use those, I just commented them out for now. They can be re-introduced as needed

--- Models/SlackActionRequest.cs
+++ Models/SlackActionRequest.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 namespace Ticketer.Models {
     public class SlackActionRequest {
         public IEnumerable<SlackAction> actions {get; set;}
-        public string callback_id {get; set;}
-        public string team {get; set;}
-        public string channel {get; set;}
-        public string user {get; set;}
-        public string action_ts {get; set;}
-        public string message_ts {get; set;}
-        public string attachment_id {get; set;}
-        public string token {get; set;}
-        //original_message can be implemented if needed
-        public string response_url {get; set;}
+        // public string callback_id {get; set;}
+        // public string team {get; set;}
+        // public string channel {get; set;}
+        // public string user {get; set;}
+        // public string action_ts {get; set;}
+        // public string message_ts {get; set;}
+        // public string attachment_id {get; set;}
+        // public string token {get; set;}
+        // //original_message can be implemented if needed
+        // public string response_url {get; set;}
     }
 }
